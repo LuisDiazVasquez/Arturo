71be671df30d1ef7ceb5efe9828eb01630b880e8
LuisDiazVasquez <arturol.diazv@gmail.com>
2017-03-14 00:04:19 -0700

    Add files via upload

--- PTWOF.java
+++ PTWOF.java
@@ -6,6 +6,7 @@ public class PTWOF
         
         double bataverg = -1.0;
         double whole = -1;
+        System.out.println("Batting averages are: ");
         for(int index = 0; index < batswing.length; index++)
         {
             bataverg = ((Math.random() - 2) * 0.500);
@@ -14,6 +15,17 @@ public class PTWOF
             
             batswing[index] = whole;   // to run the random values of bataverg into the array
             System.out.println(batswing[index]);
+        }
+        int n = 0;
+        int count = 0;
+        for(double number:batswing)
+        {
+            if(number > number){
+                System.out.println("Maximum average");
+            } else {
+                count = count + 1;
+            }
+            n = n + 1;
         }
     }
 }
